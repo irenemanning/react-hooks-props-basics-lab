778d21b3bcf0aac24fe541909f25b214b6754907
Irene <irenemanningm@gmail.com>
2022-02-24 16:37:59 -0600

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,13 @@ import React from "react";
 
 
 function About(props) {
+  const biotext = props.bio
   return (
     <div id="about">
       <h2>About Me</h2>
+      {biotext.length > 0 &&
       <p>{props.bio}</p>
+      }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <div>{props.links}</div>
     </div>
